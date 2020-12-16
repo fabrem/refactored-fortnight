708979d915d67d44bb034571ff93ab762cbf863d
remi levesque <fabio@tirey.local>
2020-12-16 16:26:20 -0500

    first commit

---
+++ src/components/Headers/IndexHeader.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { Container } from "reactstrap";
+
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/antoine-barres.jpg") + ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">Rémi Lévesque</h1>
+              <div className="fog-low">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+              <div className="fog-low right">
+                <img alt="..." src={require("assets/img/fog-low.png")} />
+              </div>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+              Étudiant Génie Logiciel
+            </h2>
+          </Container>
+        </div>
+        <div
+          className="moving-clouds"
+          style={{
+            backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
+          }}
+        />
+        <h6 className="category category-absolute">
+          Fait par Rémi Lévesque - 2020
+        </h6>
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
---
+++ src/views/cv/Cv.js
@@ -0,0 +1,38 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+// nodejs library that concatenates strings
+import classnames from "classnames";
+// reactstrap components
+import {
+  Button,
+  Collapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+} from "reactstrap";
+
+export default function Cv() {
+  return (
+      
+  );
+}
