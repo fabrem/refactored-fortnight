69334da8ad276588ae2f1f24ce3dda2454f32266
remi levesque <remilevesque@remis-MacBook-Pro.local>
2020-12-28 21:49:13 -0500

    Updates

---
+++ precache-manifest.9ee12eb2ecb65b0c075ddc9bd055d5a9.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "179dac2fb95f1c5dcdada2fcb231ed80",
+    "url": "/refactored-fortnight/index.html"
+  },
+  {
+    "revision": "acbb18e9c54411d99119",
+    "url": "/refactored-fortnight/static/css/main.14886f2d.chunk.css"
+  },
+  {
+    "revision": "2d6120a8388369e9a45b",
+    "url": "/refactored-fortnight/static/js/2.4cb39422.chunk.js"
+  },
+  {
+    "revision": "620736e192a0deb669c2970427ca2289",
+    "url": "/refactored-fortnight/static/js/2.4cb39422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acbb18e9c54411d99119",
+    "url": "/refactored-fortnight/static/js/main.eda7507d.chunk.js"
+  },
+  {
+    "revision": "30f942bb7783c4b076a0017ec0876fab",
+    "url": "/refactored-fortnight/static/js/main.eda7507d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f25efaf58c622d72662",
+    "url": "/refactored-fortnight/static/js/runtime-main.9aa52e40.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/refactored-fortnight/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "7b7892780fa3ff2c3eec30916f562058",
+    "url": "/refactored-fortnight/static/media/biner.7b789278.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/refactored-fortnight/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/refactored-fortnight/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/refactored-fortnight/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "6e33796304681588245929858d77370a",
+    "url": "/refactored-fortnight/static/media/cv.6e337963.pdf"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/refactored-fortnight/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/refactored-fortnight/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/refactored-fortnight/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "176730f43499bd2cabd03664da00ba86",
+    "url": "/refactored-fortnight/static/media/golf.176730f4.jpg"
+  },
+  {
+    "revision": "efe053682ec97d330adad93c55dc7bd0",
+    "url": "/refactored-fortnight/static/media/ilya-yakover.efe05368.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/refactored-fortnight/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "e06d84cb846db2a463ac3d904ac0c362",
+    "url": "/refactored-fortnight/static/media/kaci-baum-2.e06d84cb.jpg"
+  },
+  {
+    "revision": "d65b63163c9caec8e994304bf1b1cfcc",
+    "url": "/refactored-fortnight/static/media/landing-page.d65b6316.png"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/refactored-fortnight/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/refactored-fortnight/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/refactored-fortnight/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/refactored-fortnight/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/refactored-fortnight/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "bc3fd03013c96144a2dd184b261f67da",
+    "url": "/refactored-fortnight/static/media/page1-cv.bc3fd030.png"
+  },
+  {
+    "revision": "8356fe4cdbb9627cda5e16848316f86d",
+    "url": "/refactored-fortnight/static/media/page2-cv.8356fe4c.png"
+  },
+  {
+    "revision": "ed8128994a7eed31e8ebeba831ed040c",
+    "url": "/refactored-fortnight/static/media/profile-page.ed812899.png"
+  },
+  {
+    "revision": "fe3a7838dddb1fbf6901af17ca246ea1",
+    "url": "/refactored-fortnight/static/media/uriel-soberanes.fe3a7838.jpg"
+  }
+]);
