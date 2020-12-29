cb59a6d4434a8daa2b88facc7276e7d0738e3ccc
remi levesque <remilevesque@remis-MacBook-Pro.local>
2020-12-29 10:59:10 -0500

    test

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const cspConfigPolicy = {
+    'default-src': "'none'",
+    'base-uri': "'self'",
+    'object-src': "'none'",
+    'script-src': ["'self'"],
+    'style-src': ["'self'"],
+    'img-src': "'none'"
+};
